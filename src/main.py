8e5a856d2b782592e43016e7159ee393b04bcf18
somerandomguy-coder <153010587+somerandomguy-coder@users.noreply.github.com>
2026-01-05 15:03:22 +1100

    feat: add stealthmode for task_name and description

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk, Adw, Gdk
 
 
 
@@ -12,30 +12,24 @@ class App(Adw.Application):
     def __init__(self):
         super().__init__(application_id="com.namle.Quests")
 
-        self.win = Adw.ApplicationWindow(application=self)
-        self.win.set_icon_name("task-due-symbolic")
-        self.win.set_title("Quests")
-        self.win.set_default_size(400,300)
 
         self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing = 10)
+        self.header_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.header = Adw.HeaderBar()
 
         self.add_btn = Gtk.Button(icon_name="list-add-symbolic")
-        self.add_btn.connect("clicked", self._add_task)
+        self.add_btn.connect("clicked", self._toggle_entry)
 
-        self.con = database.Database_Connection()
-        res = self.con.fetch_player()
-
-        self.level = Gtk.Label(label = res[2])
-        self.level.level_num = res[2]
+        self.level = Gtk.Label()
+        self.level.level_num = None
         
-        self.xp_label = Gtk.Label(label = f"{res[3]}/{res[4]}")
+        self.xp_label = Gtk.Label()
 
         self.xp = Gtk.ProgressBar()
-        self.xp.set_fraction(res[3]/res[4])
-        self.xp.xp_point = res[3]
+        self.xp.xp_point = None
         
-        self.player_id = res[0]
+        self.player_id = None
+        self._sync_player_stat()
 
         self.tasks_list = Gtk.ListBox()
         self.list_header = Gtk.Label(label="Task name")
@@ -50,13 +44,21 @@ class App(Adw.Application):
         self.label = Gtk.Label(label = "Welcome, Adventurer!")
         
         self.entry = Gtk.Entry(placeholder_text="Enter new quest...")
-        self.entry.connect("activate", self._add_task)
+        self.entry.connect("activate", self._toggle_description)
 
+        self.description_entry = Gtk.Entry(placeholder_text="Enter description (optional)")
+        self.description_entry.connect("activate", self._add_task)
     def do_activate(self):
+        self.win = Adw.ApplicationWindow(application=self)
+        self.win.set_icon_name("task-due-symbolic")
+        self.win.set_title("Quests")
+        self.win.set_default_size(400,300)
+        
         self.header.pack_start(self.add_btn)
 
-        self.main_box.append(self.header)
-        self.main_box.append(self.entry)
+        self.header_box.append(self.header)
+
+        self.main_box.append(self.header_box)
         self.main_box.append(self.content_box)
 
         self.content_box.append(self.label)
@@ -68,6 +70,18 @@ class App(Adw.Application):
 
         self.win.set_content(self.main_box)
         self.win.present()
+
+############################## CSS
+        provider = Gtk.CssProvider()
+        provider.load_from_data("""
+            label.level-text { font-size: 24pt; font-weight: bold; color: red; }
+            progressbar trough { min-height: 10px; border-radius: 5px; }
+            progressbar progress { background-color: #3584e4; }
+        """.encode())
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(), provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+        self.level.add_css_class("level-text")
 
     def _check_and_display_empty_list(self):
         # if the last row is also the header then the list is empty
@@ -93,10 +107,25 @@ class App(Adw.Application):
         self.con.update_player_stat(new_xp, new_level, self.player_id)
         self.con.update_complete_task(widget.id)
         
-        row = widget.get_ancestor(Adw.ActionRow)
+        row = widget.get_ancestor(Adw.ExpanderRow)
         if row:
             self.tasks_list.remove(row)
         self._check_and_display_empty_list()
+
+    def _sync_player_stat(self):
+        self.con = database.Database_Connection()
+        res = self.con.fetch_player()
+
+        self.level.set_label(str(res[2]))
+        self.level.level_num = res[2]
+        
+        self.xp_label.set_label(f"{res[3]}/{res[4]}")
+
+        self.xp.set_fraction(res[3]/res[4])
+        self.xp.xp_point = res[3]
+        
+        self.player_id = res[0]
+        
 
     def _load_task_and_display(self):
         tasks = self.con.fetch_unfinished_tasks()
@@ -104,27 +133,71 @@ class App(Adw.Application):
             self.tasks_list.append(Gtk.Label(label="No active quests. Take a rest, Adventurer!"))
         else:
             for task in tasks:
-                row = Adw.ActionRow(title=task[1])
+                row = Adw.ExpanderRow(title=task[1])
+                if task[2] == 3: # Hard
+                    row.add_css_class("error") 
+                elif task[2] == 2: # Medium
+                    row.add_css_class("warning")
+                print("diffuculty is",task[2])
+                description_text = task[3]
+                if description_text == "":
+                    description_text= "No description for this task"
+                description = Gtk.Label(label=description_text)
+                row.add_row(description)
                 finish_btn = Gtk.Button(icon_name="object-select-symbolic", label = str(task[6]))
                 finish_btn.xp_point = task[6]
                 finish_btn.id = task[0]
                 finish_btn.connect("clicked", self._calculate_and_change_display)
                 row.add_suffix(finish_btn)
                 self.tasks_list.append(row)
+                
         self.content_box.append(self.tasks_list)
     
+    def _toggle_entry(self, widget):
+        if self.entry.get_ancestor(Gtk.Box):
+            self.header_box.remove(self.entry)
+            if self.description_entry.get_ancestor(Gtk.Box):
+                self.header_box.remove(self.description_entry)
+        else:
+            self.header_box.append(self.entry)
+            self.entry.grab_focus()
+            
+    
+    def _toggle_description(self, widget):
+        if self.description_entry.get_ancestor(Gtk.Box) == None:
+            self.header_box.append(self.description_entry)
+            self.description_entry.grab_focus()
+
     def _remove_task(self):
-        self.content_box.remove(self.tasks_list)
-        self.tasks_list = Gtk.ListBox()
+        # Instead of recreating the ListBox, just empty it
+        while child := self.tasks_list.get_first_child():
+            self.tasks_list.remove(child)
+    
+        # Put the header back
         self.list_header = Gtk.Label(label="Task name")
-        self.tasks_list.append(self.list_header)
+        self.tasks_list.append(self.list_header)        
 
     def _add_task(self, widget):
         task_name = self.entry.get_text()
-        self.entry.set_text("")
-        self.con.add_new_task(self.player_id, task_name, reward_xp=20)
-        self._remove_task()
-        self._load_task_and_display() 
+        description = self.description_entry.get_text()
+        difficulty, xp = self._resolve_difficulty(task_name)
+        if task_name != "":
+            self.entry.set_text("")
+            self.description_entry.set_text("")
+            self.con.add_new_task(self.player_id, task_name, difficulty=difficulty, description=description, reward_xp=xp)
+            self._remove_task()
+            self._load_task_and_display() 
+            self._toggle_description(None)
+
+    def _resolve_difficulty(self, task_name):
+        task_name = task_name.strip()
+        if task_name.endswith("/h"):
+            return 3, 100 
+        elif task_name.endswith("/m"):
+            return 2, 50
+        else:
+            return 1, 10
+        
 
 app = App()
 app.run(sys.argv)
